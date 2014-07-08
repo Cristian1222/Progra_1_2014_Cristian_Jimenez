0af88ef7e75956afa30054ac08bd77b908660de8
Cristian1222 <cristianj_58@hotmail.com>
2014-07-08 15:45:44 -0600

    Practice Swicht

---
+++ class 5/switch/src/pkgswitch/clsOperaciones.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgswitch;
+
+/**
+ *
+ * @author profe
+ */
+public class clsOperaciones {
+
+    public double Sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double Resta(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+    public double Division(double valor1, double valor2) {
+        if (valor2 == 0) {
+            return 0;
+
+        } else {
+            return valor1 / valor2;
+        }
+    }
+
+    public double Multiplicacion(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+     public double Raiz(double valor1) {
+        return Math.sqrt(valor1);
+    }
+     
+     public double Potencia(double valor1, double valor2) {
+        return Math.pow(valor1,valor2);
+    }
+}
