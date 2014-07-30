f08d4c2fd6a9afce24088e7bec0382ae4fee2f08
Cristian1222 <cristianj_58@hotmail.com>
2014-07-30 11:02:35 -0600

    class21

---
+++ Class21/src/class21/Class21.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class21;
+
+/**
+ *
+ * @author Cristian J
+ */
+public class Class21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMain M = new frmMain();
+        M.setVisible(true);
+    }
+}
